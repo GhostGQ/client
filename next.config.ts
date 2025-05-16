7ab8bd630e8447eea4edf6fe71e0131ec31688ed
Damir <borisov.damir.2893@gmail.com>
2025-05-16 06:35:36 +0500

    Localization fix #1

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,10 @@
+import nextI18NextConfig from './next-i18next.config';
 import type { NextConfig } from "next";
-const { i18n } = require('./next-i18next.config');
 
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
-  i18n
+  i18n: nextI18NextConfig.i18n,
 };
 
 export default nextConfig;
