b2ebd8895fe72605470b1a46723dd01c04d9a283
= <=>
2025-05-23 01:58:20 +0500

    Admin refactor

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,29 +1,36 @@
-'use client';
-
 import '@/styles/globals.css';
 import '../styles/admin.css';
+import '../styles/admin-login.css';
+import '../styles/admin-dashboard.css';
+import '../styles/admin-layout.css';
+import '../styles/admin-products.css';
+import '../styles/admin-categories.css';
 import '@/styles/products.css';
 import '@/styles/requests.css';
 import 'leaflet/dist/leaflet.css';
 import '@mantine/core/styles.css';
-import type {AppProps} from 'next/app';
-import {MantineProvider} from '@mantine/core';
-import {appWithTranslation} from 'next-i18next';
-import {Manrope} from 'next/font/google';
+import type { AppProps } from 'next/app';
+import { MantineProvider } from '@mantine/core';
+import { appWithTranslation } from 'next-i18next';
+import { Manrope } from 'next/font/google';
 import Header from '@/components/header/Header';
 import i18nConfig from '../next-i18next.config';
+import { useRouter } from 'next/router';
 
 const manrope = Manrope({
-  subsets: ['latin', 'cyrillic'], // лучше указать и кириллицу для русского
-  weight: ['400', '500', '700'], // если нужно
-  variable: '--font-manrope', // по желанию
+  subsets: ['latin', 'cyrillic'],
+  weight: ['400', '500', '700'],
+  variable: '--font-manrope',
 });
 
-function App({Component, pageProps}: AppProps) {
+function App({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+  const isAdminRoute = router.pathname.startsWith('/admin');
+
   return (
     <MantineProvider>
-      <div className={manrope.className}>
-        <Header />
+      <div className={`main-wrapper ${manrope.className}`}>
+        {!isAdminRoute && <Header />}
         <Component {...pageProps} />
       </div>
     </MantineProvider>
